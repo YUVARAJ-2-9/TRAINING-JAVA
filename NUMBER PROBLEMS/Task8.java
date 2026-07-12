59d329455592d8ce655b428412cc52ae5f3e3e8c
YUVARAJ-2-9 <yuvarajyuvaraj1417@gmail.com>
2026-07-12 13:40:14 +0530

    chnages

---
+++ NUMBER PROBLEMS/Task8.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class Task8 {
+
+    public static int getGCD(int n1,int n2){
+        while(n2 != 0){
+            int rem = n1 % n2;
+            n1 = n2;
+            n2 = rem ;
+
+
+        }
+        return n1 ;
+
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the value:");
+        int n1 = sc.nextInt();
+
+        System.out.println("Enter the value:");
+        int n2 = sc.nextInt();
+
+        
+        System.out.println(getGCD(n1, n2));
+
+
+        
+
+       
+        
+
+       
+
+        }
+        
+        
+
+
+    }
+
