f4debd61cbbc8ddf176ed24f9dd9a143efc6c0d6
YUVARAJ-2-9 <yuvarajyuvaraj1417@gmail.com>
2026-07-14 15:56:25 +0530

    chnges

---
+++ ARRRAY PROBLEMS/Array4.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Array4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size:");
+        int size = sc.nextInt();
+        int [] arr = new int[size];
+        System.out.println("Array values:" );
+
+        for(int i =0;i<arr.length;i++){
+            arr[i] = sc.nextInt();
+        }
+
+        int min = arr[0];
+        for(int i =0;i<arr.length;i++){
+            if(arr[i] < min){
+                min = arr[i];
+
+            }
+        }
+        System.out.println("MINIMUM VALUES: " + min);
+
+    }
+}
